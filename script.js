7066898ee6a304220af341f54d3a3d98f68250b8
zpdh <36316038+zpdh@users.noreply.github.com>
2024-04-11 16:38:00 -0300

    made a simple, static, 16x16 grid.

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-let slider = document.getElementById('range')
-let gridSize = document.getElementById('gridsize')
+
+//Slider 
+const slider = document.getElementById('range')
+const gridSize = document.getElementById('gridsize')
 gridSize.textContent = `Grid Size: ${slider.value}x${slider.value}`
 
 slider.oninput = function() {
     gridSize.textContent = `Grid Size: ${this.value}x${this.value}`
 }
+
+//Grid
+const gridContent = document.getElementById('gridContent')
+const rows = document.getElementsByClassName('gridRow')
+const cells = document.getElementsByClassName('gridCell')
+
+function makeGrid() {
+    makeRows()
+    makeCells()
+}
+
+function makeRows() {
+for (i = 0; i < slider.value ;i++) {
+    const createRow = document.createElement('div')
+    gridContent.appendChild(createRow).className = 'gridRow'
+}}
+
+function makeCells() {
+    for (i = 0; i < rows.length ; i++) {
+        for (j = 0; j < slider.value; j++) {
+        const createCell = document.createElement('div')
+        gridContent.appendChild(createCell).className = 'gridCell'
+    }}}
+
+addEventListener('load', () => {makeGrid()})
+
+
