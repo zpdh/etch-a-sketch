0a358168b4c5d9de138aa72e86c4543198caec16
zpdh <36316038+zpdh@users.noreply.github.com>
2024-04-13 02:04:49 -0300

    grid cells no longer have a static size and vary based on slider value

--- script.js
+++ script.js
@@ -3,19 +3,25 @@
 const slider = document.getElementById('range')
 const gridSize = document.getElementById('gridsize')
 gridSize.textContent = `Grid Size: ${slider.value}x${slider.value}`
-
+let thisvalue = slider.value
+let cellSize = 480 / slider.value
 slider.oninput = function() {
+    thisvalue = this.value
     gridSize.textContent = `Grid Size: ${this.value}x${this.value}`
+    cellSize = 480 / thisvalue
 }
 
 //Grid
 const gridContent = document.getElementById('gridContent')
 const rows = document.getElementsByClassName('gridRow')
-const cells = document.getElementsByClassName('gridCell')
-
+const cells = document.getElementById('gridCell')
 function makeGrid() {
     makeRows()
     makeCells()
+    let allCells = document.querySelectorAll('#gridCell')
+    allCells.forEach( cell => {
+        cell.style.cssText += `width:${cellSize}px;height:${cellSize}px;`
+    })
 }
 
 function makeRows() {
@@ -28,7 +34,7 @@ function makeCells() {
     for (i = 0; i < rows.length ; i++) {
         for (j = 0; j < slider.value; j++) {
         const createCell = document.createElement('div')
-        gridContent.appendChild(createCell).className = 'gridCell'
+        gridContent.appendChild(createCell).id = 'gridCell'
     }}}
 
 addEventListener('load', () => {makeGrid()})
